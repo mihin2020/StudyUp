fc3ef4c23a98ae4bdad3ebc115f56967f15442cf
Aboubakary833 <aboubakarycisse472@gmail.com>
2021-07-27 11:22:53 +0000

    Branche à Cissé

--- app/Http/Controllers/Auth/auth.php
+++ app/Http/Controllers/Auth/auth.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [LoginController::class, 'index'])->name('login.index');
 Route::post('/login', [LoginController::class, 'create'])->name('login.create');
-Route::resource('/register', 'RegisterController');
+Route::get('/register', [RegisterController::class, 'index'])->name('register.index');
+Route::post('/register', [RegisterController::class, 'create'])->name('register.create');
