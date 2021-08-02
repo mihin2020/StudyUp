c34ec1884af6d98e41ff43412ccd66ef42e32974
mihin21 <mihin.aime@gmail.com>
2021-08-02 11:52:26 +0200

    reglage

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\CategoriesController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
