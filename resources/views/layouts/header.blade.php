75799f7f3cf3a0ecc30599e6260f8fa24d558c5c
Aboubakary833 <aboubakarycisse472@gmail.com>
2021-08-02 15:49:00 +0000

    Modification du formulaire de login

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,8 +1,23 @@
-<header>
-    <nav class="navbar navbar-light bg-smt-primary py-0">
-        <a class="navbar-brand d-flex align-items-center" href="#">
-          <img src="{{asset('dist/img/logo.png')}}" width="50" height="50" class="d-inline-block align-top bg-light rounded-circle mr-2" alt="">
-          <span class="h3 text-white font-weight-bold">StudyUp</span>
-        </a>
-      </nav>
+<header class="navbar navbar-light d-flex bg-smt-primary py-0">
+    <a class="navbar-brand d-flex align-items-center" href="#">
+        <img src="{{asset('dist/img/logo.png')}}" width="50" height="50" class="d-inline-block align-top bg-light rounded-circle mr-2" alt="">
+        <span class="h3 text-white font-weight-bold">StudyUp</span>
+    </a>
+    <ul class="nav justify-content-end header-nav">
+        <li class="nav-item">
+          <a class="nav-link font-weight-bold active" href="#">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link font-weight-bold" href="#">FAQs</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link font-weight-bold" href="#">Contacter</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link font-weight-bold" href="#">À propos</a>
+          </li>
+        <li class="nav-item">
+          <a class="nav-link font-weight-bold" href="#">Se connecter</a>
+        </li>
+    </ul>
 </header>
